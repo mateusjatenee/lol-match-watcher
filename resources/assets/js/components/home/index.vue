0f9a48e4e7049be29513d7481081b19301c0a65b
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-13 01:23:04 -0300

    mionr changes

--- resources/assets/js/components/home/index.vue
+++ resources/assets/js/components/home/index.vue
@@ -1,7 +1,33 @@
-<style>
-	
+<style lang="stylus">
+
+.__banner
+	background: #744d82;
+	background: -moz-radial-gradient(top,ellipse cover,#744d82 11%,#503d67 99%,#503d67 100%);
+	background: -webkit-gradient(radial,top center,0,center center,100%,color-stop(11%,#744d82),color-stop(99%,#503d67),color-stop(100%,#503d67));
+	background: -webkit-radial-gradient(top,ellipse cover,#744d82 11%,#503d67 99%,#503d67 100%);
+	background: -o-radial-gradient(top,ellipse cover,#744d82 11%,#503d67 99%,#503d67 100%);
+	background: -ms-radial-gradient(top,ellipse cover,#744d82 11%,#503d67 99%,#503d67 100%);
+	background: radial-gradient(ellipse at top,#744d82 11%,#503d67 99%,#503d67 100%);
+	height 100vh
+	.__hero
+		padding: 22rem 0
+
+		.hero-header
+			font-size 50px
+			color #fff
+
+
+
 </style>
 
 <template>
-	<p>Teste</p>
+	<div class="__banner">
+		<div class="container __hero">
+			<div class="row">
+				<div class="one column">
+					<h1 class="hero-header">Whatever</h1>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
