cdfca21c784747da77474734e3ce4b6ca4195617
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-13 01:01:13 -0300

    import component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,9 +2,13 @@ var Vue = require('vue');
 Vue.use(require('vue-resource'));
 
 import LoadingSpinner from './components/LoadingSpinner.vue';
+import Index from './components/home/index.vue';
 
 Vue.component('loading-spinner', LoadingSpinner);
 
 new Vue({
 	el: 'body',
+	components: {
+		Index,
+	}
 });
